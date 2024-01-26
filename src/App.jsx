eacfff882c61fb04f33b0f41f38e1abcf6488289
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-01-26 16:45:23 -0500

    Navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './main.css'
+import Navbar from './Components/navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
     <h1>Hello welcome to my channel. I will be teaching you how to make a website using brainf*ck</h1>
     </>
   )
---
+++ src/Components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul className='flex px-4 gap-10 bg-red-400'>
+            <li>Home</li>
+            <li>Category</li>
+            <li>About</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
